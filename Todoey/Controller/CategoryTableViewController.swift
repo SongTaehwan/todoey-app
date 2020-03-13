89f2b77773edf2903cb0d38f59fb19db77873221
SongTaehwan <axoghks@gmail.com>
2020-03-13 16:20:02 +0900

    Change Data storage with CoreDate and Built CategoryTableViewController

---
+++ Todoey/Controller/CategoryTableViewController.swift
@@ -0,0 +1,94 @@
+//
+//  CategoryTableViewController.swift
+//  Todoey
+//
+//  Created by 송태환 on 2020/03/13.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryTableViewController: UITableViewController {
+    var categoryArray = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadCategories()
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let category = categoryArray[indexPath.row]
+        cell.textLabel?.text = category.name
+
+        return cell
+    }
+    
+    // MARK: - Table view delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("clicked")
+        tableView.cellForRow(at: indexPath)?.setSelected(false, animated: true)
+        performSegue(withIdentifier: "CategoryToItem", sender: self)
+    }
+    
+    // MARK: - Add New Category
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField: UITextField?
+        
+        let alert = UIAlertController(title: "Add Category", message: "", preferredStyle: .alert)
+    
+        alert.addTextField { (alertTextField) in
+            textField = alertTextField
+            textField?.placeholder = "Type New Category"
+        }
+    
+        let alertAction = UIAlertAction(title: "Add", style: .default) { (action) in
+            if let name = textField?.text {
+                let newCategory = Category(context: self.context)
+                newCategory.setValue(name, forKey: "name")
+                self.categoryArray.append(newCategory)
+                
+                self.saveCategory()
+            }
+        }
+        
+        alert.addAction(alertAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - Core Data method
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            categoryArray = try context.fetch(request)
+            tableView.reloadData()
+        } catch  {
+            print(error)
+        }
+    }
+
+    func saveCategory() {
+        do {
+            try context.save()
+            tableView.reloadData()
+        } catch  {
+            print(error)
+        }
+    }
+
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "CategoryToItem" {
+            let destination = segue.destination as! TableViewController
+            let indexPath = self.tableView.indexPathForSelectedRow!
+            destination.categoryName = categoryArray[indexPath.row].name
+        }
+    }
+}
--- Todoey/Controller/TableViewController.swift
+++ Todoey/Controller/TableViewController.swift
@@ -7,30 +7,25 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
-    // CAN NOT Store Custom data or Object, will throw non-property list erorr
-//    let defaults = UserDefaults()
-    
-    // init with type
     var itemArray = [Todo]()
-    
-    // Create Custom plist
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Todo.plist")
+    var categoryName: String?
+
+    // get DB instance
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         loadTodos()
-//        if let array = defaults.array(forKey: "TodoList") as? [Todo] {
-//            itemArray = array
-//        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath)
         
@@ -48,7 +43,9 @@ class TableViewController: UITableViewController {
         let todo = itemArray[indexPath.row]
 
         // clicked -> change done state
-        todo.done = !todo.done
+//        todo.done = !todo.done
+        // MARK: - CoreData Update
+        todo.setValue(!todo.done, forKey: "done")
         
         saveTodo()
 
@@ -62,17 +59,14 @@ class TableViewController: UITableViewController {
 
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen once user clicks the Add Item button on Alert
-            let newItem = Todo()
-            newItem.title = textField.text!
+            // MARK: - CoreData Create
+            let newItem = Todo(context: self.context)
+//            newItem.title = textField.text!
+            newItem.setValue(textField.text!, forKey: "title")
+
             self.itemArray.append(newItem)
 
             self.saveTodo()
-            
-            // store data like AsyncStorage
-            // DO NOT Store Object or Custom data in UserDefaults, it will crash app and it's desgined to store a small piece of data
-//            self.defaults.set(self.itemArray, forKey: "TodoList")
-
-    
         }
         
         // MARK: - Callback triggers once TextField created
@@ -87,39 +81,64 @@ class TableViewController: UITableViewController {
     }
     
     func saveTodo() -> Void {
-        let encoder = PropertyListEncoder()
-
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
-            
-            // reload for UI
+            try context.save()
             tableView.reloadData()
         } catch {
             print(error)
         }
     }
     
-    func loadTodos() -> Void {
-        // option 1
-//        do {
-//            let data = try Data(contentsOf: dataFilePath!)
-//            let decoder = PropertyListDecoder()
-//            itemArray = try decoder.decode([Todo].self, from: data)
-//        } catch {
-//            print(error)
-//        }
-        
-        // option 2
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            
-            do {
-                itemArray = try decoder.decode([Todo].self, from: data)
-            } catch {
-                print(error)
-            }
+    func loadTodos(with request: NSFetchRequest<Todo> = Todo.fetchRequest()) -> Void {
+        do {
+            // MARK: - CoreData Read
+            // below two lines of codes work the same
+//            let request: NSFetchRequest<Todo> = Todo.fetchRequest()
+//            let request = NSFetchRequest<Todo>(entityName: "Todo")
+            request.predicate = NSPredicate(format: "parentCategory == %@", self.categoryName!)
+            itemArray = try context.fetch(request)
+            tableView.reloadData()
+        } catch {
+            print(error)
         }
     }
 }
 
+// MARK: - UISearchBarDelegate
+extension TableViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // Create request instance
+        let request: NSFetchRequest<Todo> = Todo.fetchRequest()
+
+        // Add Query
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.predicate = predicate
+
+        // Add sort conditions which can be multiple condition
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        request.sortDescriptors = [sortDescriptor]
+
+        // refactor -> loadTodos
+//        do {
+//            itemArray = try context.fetch(request)
+//            tableView.reloadData()
+//        } catch {
+//            print(error)
+//        }
+        
+        loadTodos(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadTodos()
+            
+            // UI changes should be running on Main thread even though background thread is completed
+            DispatchQueue.main.async {
+                // get searchBar back to origin state (no keyboard popup and cursor blink)
+                searchBar.resignFirstResponder()
+            }
+            
+        }
+    }
+}
